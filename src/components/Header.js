f320278350a5f247755d1e6e7d17c322bce0e749
lhia16 <lhia_gregg@hotmail.co.uk>
2020-12-02 17:48:23 +0000

    Made and styled the header component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css';
+import {Link} from 'react-router-dom';
+import Twitter from './twitter.jpg'
+import Icon from './quill.jpg'
+
+const Header = () => {
+        return (
+            <div className="header">
+                <img className="bird"src={Twitter} alt="bird" />
+                <h1>Home</h1>
+                <img className="icon"src={Icon} alt="tweet" />
+            </div>
+        )
+    };
+    
+    export default Header;
+  
