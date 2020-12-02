18ed7c2b9d4a6f63c124acbdb363d0f6f19b20d6
Joseph D'ambra <josephdambra@Josephs-MacBook-Pro.local>
2020-12-02 17:42:29 +0000

    nav bar

---
+++ src/components/Notifications.js
@@ -0,0 +1,11 @@
+
+const Notifications = () => {
+    return (
+        <div>
+            <h1>No New Notifications</h1>
+            <p>You are fully up to date!</p>
+        </div>
+    )
+}
+
+export default Notifications;
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+
+const Search = () => {
+    return (
+    <div>
+        <h1>Search Here</h1>
+        <input type="text"></input>
+    </div>
+    )
+}
+
+export default Search;
