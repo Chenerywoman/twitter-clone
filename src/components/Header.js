31ad88d0c007ffc788c14db29418c62a260c3e28
lhia16 <lhia_gregg@hotmail.co.uk>
2020-12-03 14:06:06 +0000

    added toggle for visibility

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Header.css';
 import {Link} from 'react-router-dom';
 import Twitter from './twitter.jpg'
 import Icon from './quill.jpg'
 
+
+
 const Header = () => {
+    const [visibility, setVisibility] = useState(false);
+
+    const toggleVisibility = () => {
+        if (visibility == false) {
+            setVisibility(true);
+        } else {
+            setVisibility(false);
+        }
+        console.log(visibility);
+    }
         return (
             <div className="header">
                 <img className="bird"src={Twitter} alt="bird" />
                 <h1>Home</h1>
-                <img className="icon"src={Icon} alt="tweet" />
+                <img onClick={toggleVisibility} className="icon"src={Icon} alt="tweet" />
             </div>
         )
     };
