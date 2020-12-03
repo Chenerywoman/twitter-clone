81495b2b3b91dab38440faf15311769171634641
lhia16 <lhia_gregg@hotmail.co.uk>
2020-12-03 15:45:00 +0000

    adding tweet object

--- src/components/Inputbox.js
+++ src/components/Inputbox.js
@@ -2,17 +2,35 @@ import React, {useState} from 'react'
 
 
 function Inputbox() {
-    const [tweet, setTweet] = useState("");
+    const [input, setInput] = useState("");
+
+    const [completedTweet, setCompletedTweet] = useState({
+        name: "twittermaniac",
+        username: "@twittermaniac",
+        userpic: "https://en.bcdn.biz/Images/2016/11/15/776342f0-86f5-4522-84c9-a02d6b11c766.jpg",
+        tweet: "",
+        pic: "",
+        alt: "",
+        like: false
+    });
 
     const handleTweet = (e) => {
-        setTweet (e.target.value)
-        console.log(`handle${tweet}`)
+        setInput (e.target.value)
+        console.log(`handle${input}`)
 
     }
 
     const handleSubmit = (e) => {
-        console.log(`submit${tweet}`)
-        setTweet("")
+        console.log(`submit${input}`)
+        setCompletedTweet({...completedTweet, tweet: input
+        })
+
+        console.log(completedTweet);
+
+
+
+
+        setInput("")
 
 
         e.preventDefault()
@@ -21,7 +39,7 @@ function Inputbox() {
 
     return (
         <form onSubmit={handleSubmit}>
-            <input value={tweet} onChange={handleTweet} type="text" />
+            <input value={input} onChange={handleTweet} type="text" />
             <button type="submit">Tweet</button>
 
             
