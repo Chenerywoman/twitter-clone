d28e664f571c07bdabde3e41da0da7c6c3046b20
Chenerywoman <rachelmchenery@hotmail.com>
2020-12-03 20:48:29 +0000

    chaning to receive props from TwitterHolder

--- src/components/Inputbox.js
+++ src/components/Inputbox.js
@@ -1,52 +1,15 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 
-function Inputbox() {
-    const [input, setInput] = useState("");
-
-    const [completedTweet, setCompletedTweet] = useState({
-        name: "twittermaniac",
-        username: "@twittermaniac",
-        userpic: "https://en.bcdn.biz/Images/2016/11/15/776342f0-86f5-4522-84c9-a02d6b11c766.jpg",
-        tweet: "",
-        pic: "",
-        alt: "",
-        like: false
-    });
-
-    const handleTweet = (e) => {
-        setInput (e.target.value)
-        console.log(`handle${input}`)
-
-    }
-
-    const handleSubmit = (e) => {
-        console.log(`submit${input}`)
-        setCompletedTweet({...completedTweet, tweet: input
-        })
-
-        console.log(completedTweet);
-
-
-
-
-        setInput("")
-
-
-        e.preventDefault()
-
-    }
-
+function Inputbox({input, handleSubmit, handleTweet}) {
+   
     return (
         <form onSubmit={handleSubmit}>
             <input value={input} onChange={handleTweet} type="text" />
             <button type="submit">Tweet</button>
-
-            
-
-            
         </form>
     )
+    
 }
 
 export default Inputbox
