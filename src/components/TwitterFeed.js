cae53d725b7bf24e9132b58b938655d737ae9db3
Chenerywoman <rachelmchenery@hotmail.com>
2020-12-04 13:30:56 +0000

    passing in like and updateLike to Tweet

--- src/components/TwitterFeed.js
+++ src/components/TwitterFeed.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Tweet from './Tweet';
 
-const TwitterFeed = ({tweets}) => {
+const TwitterFeed = ({tweets, updateLike}) => {
 
     return (
         <div className="twitter-feed">
             {tweets.map((tweet, ind) => {
+                console.log(`like in twitterFeed ${tweet.like}`)
                 return (
                     <Tweet 
                         key={ind}
+                        ind={ind}
                         name={tweet.name}
                         username={tweet.username}
                         userpic={tweet.userpic}
                         tweet={tweet.tweet}
                         pic={tweet.pic}
                         alt={tweet.alt}
+                        like={tweet.like}
+                        updateLike={updateLike}
                     />
                 )
             })}
