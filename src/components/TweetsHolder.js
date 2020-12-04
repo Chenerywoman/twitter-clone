60f6a18b10204438efc2ce4aa6ce7c1a43adc313
Chenerywoman <rachelmchenery@hotmail.com>
2020-12-04 12:00:50 +0000

    adding updateLike

--- src/components/TweetsHolder.js
+++ src/components/TweetsHolder.js
@@ -14,7 +14,7 @@ const TweetsHolder = () => {
         username: "@twittermaniac",
         userpic: "https://en.bcdn.biz/Images/2016/11/15/776342f0-86f5-4522-84c9-a02d6b11c766.jpg",
         tweet: "",
-        pic: "",
+        pic: "https://peliproducts.co.uk/wp/wp-content/uploads/2018/01/2-Northern-Quarter-1.jpg",
         alt: "",
         like: false
     });
@@ -33,7 +33,13 @@ const TweetsHolder = () => {
 
     }
 
-    console.log(tweets)
+    const updateLike = (ind, likeStatus) => {
+        let updatedTweets = [...tweets];
+        updatedTweets[ind].like = likeStatus;   
+        setTweets([...updatedTweets])
+
+    }
+
     return (
         <div>
             <InputBox 
@@ -41,7 +47,10 @@ const TweetsHolder = () => {
                 handleSubmit={handleSubmit}
                 handleTweet={handleTweet}
             />
-            <TwitterFeed tweets={tweets}/>
+            <TwitterFeed 
+            tweets={tweets}
+            updateLike={updateLike}
+            />
         </div>
     )
 }
